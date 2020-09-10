641603ca7df26a4add2c23d24a60fbbf7894f573
Dean Whillier <dean@whillier.com>
2020-09-10 09:49:28 -0400

    initial commit
    - lerna
    - compass icon font package

---
+++ packages/compass-icons/generate-config.js
@@ -0,0 +1,17 @@
+import { getFileData, generateGlyphs, writeFileToDisk } from "./utils";
+
+const files = getFileData();
+
+const glyphs = generateGlyphs(files);
+
+const configData = {
+    name: 'compass-icons',
+    css_prefix_text: 'icon-',
+    css_use_suffix: false,
+    hinting: false,
+    units_per_em: 1000,
+    ascent: 850,
+    glyphs
+}
+
+writeFileToDisk('config.json', configData);
